40d40d804c27d86a89acf36dff510c76cae4b217
prakhyat798 <prakhyatmaurya58@gmail.com>
2026-06-11 11:11:18 +0530

    Add Singly Linked List

---
+++ 5.java
@@ -0,0 +1,47 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    public ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+public class SinglyLinkedList {
+    ListNode head;
+    ListNode tail;
+
+    public SinglyLinkedList() {
+        head = new ListNode(-1);
+        tail = head;
+    }
+
+    public void insertEnd(int val) {
+        tail.next = new ListNode(val);
+        tail = tail.next; 
+    }
+
+    public void remove(int index) {
+        int i = 0;
+        ListNode curr = head;
+        while (i < index && curr != null) {
+            i++;
+            curr = curr.next;
+        }
+        
+        if (curr != null) {
+            curr.next = curr.next.next;
+        }
+    }
+
+    public void print() {
+        ListNode curr = head.next;
+        while (curr != null) {
+            System.out.print(curr.val + " -> ");
+            curr = curr.next;
+        }
+
+        System.out.println();
+    }
+}
